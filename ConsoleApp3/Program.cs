fef449fe78e65f42cef13786eacee08517cc5cb1
mazenabdelkader <168626481+mazenabdelkader@users.noreply.github.com>
2025-01-30 18:33:05 +0200

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 
 namespace ConsoleApp3
 {
@@ -121,47 +121,7 @@ namespace ConsoleApp3
 
         #region 5
 
-public class FixedSizeList<T>
-    {
-        private T[] items;
-        private int count;
-
-        public int Capacity { get; private set; }
-
-        public FixedSizeList(int capacity)
-        {
-            if (capacity <= 0)
-            {
-                throw new ArgumentException("more than 0.");
-            }
-
-            Capacity = capacity;
-            items = new T[capacity];
-            count = 0;
-        }
-
-        public void Add(T item)
-        {
-            if (count >= Capacity)
-            {
-                throw new InvalidOperationException("List is full.");
-            }
-
-            items[count] = item;
-            count++;
-        }
-
-        public T Get(int index)
-        {
-            if (index < 0 || index >= count)
-            {
-                throw new IndexOutOfRangeException("Invalid index.");
-            }
-
-            return items[index];
-        }
-    }
-
+??????????????????
 
 
 
